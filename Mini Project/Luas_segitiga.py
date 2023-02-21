0df0407aa615503f750aeb38fcab71cae51d3cb9
Syafiq1331 <bussinesyafiq@gmail.com>
2023-02-21 14:42:28 +0700

    "add mini project | Menghitung Luas Segitiga"

---
+++ Mini Project/Luas_segitiga.py
@@ -0,0 +1,8 @@
+# Function
+
+def luas_segitiga(alas, tinggi):
+    luas = alas * tinggi / 2
+    print ("Luas segitiga dengan alas {} dan tinggi {} adalah {}".format(alas, tinggi, luas))
+
+# Main Program
+luas_segitiga(10, 6)
